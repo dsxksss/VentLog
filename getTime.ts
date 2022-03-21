ba2e872fffcdc35788585bcaa61e33bff3471524
dsxk <2546650292@qq.com>
2022-03-21 21:12:56 +0800

    add:获取时间

---
+++ getTime.ts
@@ -0,0 +1,28 @@
+const getTime = (): string => {
+  const date = new Date();
+  let month: string | number = date.getMonth() + 1;
+  let strDate: string | number = date.getDate();
+
+  if (month <= 9) {
+    month = "0" + month;
+  }
+
+  if (strDate <= 9) {
+    strDate = "0" + strDate;
+  }
+
+  return (
+    date.getFullYear() +
+    "-" +
+    month +
+    "-" +
+    strDate +
+    " " +
+    date.getHours() +
+    ":" +
+    date.getMinutes() +
+    ":" +
+    date.getSeconds()
+  );
+};
+export default getTime;
