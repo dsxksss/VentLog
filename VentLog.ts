dc8e684d4d3c3659f2963a8ace02ca6813a710f4
dsxk <2546650292@qq.com>
2022-03-21 18:32:04 +0800

    add:日志类

--- VentLog.ts
+++ VentLog.ts
@@ -1,9 +1,7 @@
-enum ELogLevel {
-  "Error" = 0,
-  "Warning",
-  "Info",
-}
-class Log {
+import ELogLevel from "./ELogLevel.ts";
+import { readLog, writeLog } from "./logFile.ts";
+class VentLog {
+  private fileName = "VentLog.log";
   private level: ELogLevel = ELogLevel.Info;
   public constructor() {
     this.level = ELogLevel.Info; //默认设置为INFO输出级别
@@ -12,21 +10,35 @@ class Log {
     this.level = logEvel;
   }
   public error(msg: string): void {
-    if (this.level >= ELogLevel.Error)
-      console.log("\u001b[31m[Error]:", msg, "\u001b[0m");
+    if (this.level >= ELogLevel.Error) {
+      const data = `[Error]: ${msg}`;
+      writeLog(this.fileName, data);
+      console.log("\u001b[31m", data, "\u001b[0m");
+    }
   }
   public warning(msg: string): void {
-    if (this.level >= ELogLevel.Warning)
-      console.log("\u001b[33m[Warning]:", msg, "\u001b[0m");
+    if (this.level >= ELogLevel.Warning) {
+      const data = `[Warning]: ${msg}`;
+      writeLog(this.fileName, data);
+      console.log("\u001b[33m", data, "\u001b[0m");
+    }
   }
   public info(msg: string): void {
-    if (this.level >= ELogLevel.Info)
-      console.log("\u001b[34m[Info]:", msg, "\u001b[0m");
+    if (this.level >= ELogLevel.Info) {
+      const data = `[Info]: ${msg}`;
+      writeLog(this.fileName, data);
+      console.log("\u001b[34m", data, "\u001b[0m");
+    }
+  }
+  public showLog(): void {
+    readLog(this.fileName);
   }
 }
 
-const obj = new Log();
-obj.setLevel(ELogLevel.Info);
-obj.error("我是错误信息");
-obj.warning("我是警告信息");
-obj.info("我是重要信息");
+export default VentLog;
+
+// const obj = new Log();
+// obj.setLevel(ELogLevel.Info);
+// obj.error("我是错误信息");
+// obj.warning("我是警告信息");
+// obj.info("我是重要信息");
