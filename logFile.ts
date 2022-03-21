0cad1f125ae773477af303d60add8afa692154fb
dsxk <2546650292@qq.com>
2022-03-21 18:31:37 +0800

    add:日志文件的读写操作(待优化)

---
+++ logFile.ts
@@ -0,0 +1,16 @@
+const readLog = async (fileName: string) => {
+  const filename = fileName;
+  const fileCode = new TextDecoder("utf-8");
+  const data = await Deno.readFile(filename);
+  console.log(fileCode.decode(data));
+};
+
+const writeLog = async (fileName: string, data: string) => {
+  const filename = fileName;
+  const fileWrite = new TextEncoder();
+  await Deno.writeFile(filename, fileWrite.encode(`${data} \t\n`), {
+    append: true,
+  });
+};
+
+export { readLog, writeLog };
