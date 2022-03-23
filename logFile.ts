21d2c75ca56b219d27794406ef9fe30ae0f974b6
图灵猫 <9554230+xiao-kang-zhang@user.noreply.gitee.com>
2022-03-23 16:02:48 +0800

    add:注释

--- logFile.ts
+++ logFile.ts
@@ -2,6 +2,10 @@ import getTime from "./getTime.ts";
 
 const readLog = async (fileName: string) => {
   const filename = fileName;
+  /*
+  另一种方法是直接用Deno.readTextFile(<文件名>);
+  此方法不需要创建解码器,自动读取可读的text类的编码文件
+  */
   //创建utf-8解码器
   const fileDecoder = new TextDecoder("utf-8");
   const data = await Deno.readFile(filename);
